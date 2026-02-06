5e396a126169ef4cd468f870d33d8730c35f1527
Ahmed Raza <arahmed212214@gmail.com>
2026-02-06 18:29:39 +0500

    v.2.9 Ahmed-Raza 6:29 Pm 6/2/26

---
+++ assests/js/menu-bar-visivle.js
@@ -0,0 +1,11 @@
+// // Add this to your main.js or a <script> tag
+// const navbar = document.querySelector('.navbar-collapse');
+// const header = document.querySelector('.header-section');
+
+// navbar.addEventListener('shown.bs.collapse', () => {
+//     header.classList.add('menu-open');
+// });
+
+// navbar.addEventListener('hidden.bs.collapse', () => {
+//     header.classList.remove('menu-open');
+// });
