704ce2fed2c94fde9443f384620ba13dada8c26d
Ahmed Raza <arahmed212214@gmail.com>
2026-02-12 18:02:48 +0500

    v.16.3 Ahmed-Raza 6:02 Pm 12/2/26

---
+++ assests/js/rental-dock-location.js
@@ -0,0 +1,14 @@
+let items = document.querySelectorAll('#boatCarousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 0
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
